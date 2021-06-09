7221479b62ac15ccf40d78f65047780f80479633
artempolcreator <artempolienko123@gmail.com>
2021-06-09 15:46:43 +0300

    Initial commit

---
+++ src/main/java/com/spring/mvc/dao/EmployeeDAO.java
@@ -0,0 +1,11 @@
+package com.spring.mvc.dao;
+
+import com.spring.mvc.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    public List<Employee> getAllEmployees();
+
+}
---
+++ src/main/java/com/spring/mvc/dao/EmployeeDAOImpl.java
@@ -0,0 +1,32 @@
+package com.spring.mvc.dao;
+
+import com.spring.mvc.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Transactional
+    @Override
+    public List<Employee> getAllEmployees() {
+
+        Session session = sessionFactory.getCurrentSession();
+
+        Query<Employee> query = session.createQuery("from Employee"
+                , Employee.class);
+        List<Employee> employees = query.getResultList();
+
+        return employees;
+
+    }
+}
